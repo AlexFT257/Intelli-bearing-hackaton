0c4edfc260680defa5e7d59f509bf1ea5f227851
AlexFT257 <cualquierw@gmail.com>
2024-05-16 17:47:00 -0400

    v0.3.0: dashboard page done

---
+++ Frontend/src/app/simulation/page.jsx
@@ -0,0 +1,31 @@
+"use client"
+import Image from "next/image";
+import CardTable from "@/components/Cards/CardTable";
+import CardAnalysis from "@/components/Cards/CardAnalysis";
+import CardState from "@/components/Cards/CardState";
+import Chat from "@/components/Chat";
+
+export default function Home() {
+  return (
+    <div className="flex h-full  w-full m-2">
+      {/* izquierda */}
+      <div className="flex flex-col gap-4 w-full mx-2 my-4">
+        {/* arriba */}
+        <div className="flex w-full">
+          {/* carta */}
+          <CardTable/>
+        </div>
+        {/* abajo */}
+        <div className="flex flex-row gap-4">
+          <CardAnalysis/>
+          {/* <CardAnalysis/> */}
+          <CardState/>
+        </div>
+      </div>
+      {/* derecha /chat */}
+      <div className="flex w-2/5  h-full mx-2 my-4">
+        <Chat/>
+      </div>
+    </div>
+  );
+}
