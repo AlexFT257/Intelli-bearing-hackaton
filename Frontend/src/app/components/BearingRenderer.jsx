5f81ab41e95738f835ff476703abbd894d5edbbb
Felipe Vásquez <minelyne@gmail.com>
2024-05-16 17:06:19 -0400

    canvas_size

--- Frontend/src/app/components/BearingRenderer.jsx
+++ Frontend/src/app/components/BearingRenderer.jsx
@@ -3,12 +3,13 @@ import { Bearing } from "./Bearingv2";
 import { OrbitControls } from "@react-three/drei";
 
 export function BearingRenderer() {
+  const canvasSize = 260;
   return (
-    <div>
+    <div style={{ with: canvasSize, height: canvasSize }}>
       <Canvas>
         <ambientLight intensity={1} />
         <directionalLight position={[2, 3, 2]} intensity={1} />
-        <OrbitControls autoRotate autoRotateSpeed={4}/>
+        <OrbitControls autoRotate autoRotateSpeed={4} />
         <Bearing />
       </Canvas>
     </div>
