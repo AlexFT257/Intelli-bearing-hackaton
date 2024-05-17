9760bf78ef4fb69ea1d0c1f06aa3dca7c2bc2e94
Felipe Vásquez <minelyne@gmail.com>
2024-05-17 05:33:05 -0400

    better twin

--- Frontend/src/components/BearingRenderer.jsx
+++ Frontend/src/components/BearingRenderer.jsx
@@ -3,15 +3,18 @@ import { Bearing } from "./Bearingv2";
 import { OrbitControls } from "@react-three/drei";
 
 export function BearingRenderer() {
-
-  
   const canvasSize = 260;
   return (
     <div style={{ with: canvasSize, height: canvasSize }}>
       <Canvas>
         <ambientLight intensity={1} />
-        <directionalLight position={[2, 3, 2]} intensity={1} />
-        <OrbitControls autoRotate autoRotateSpeed={4} />
+        <directionalLight position={[0, 3, 0]} intensity={1} />
+        <directionalLight position={[0, -3, 0]} intensity={1} />
+        <directionalLight position={[3, 0, 0]} intensity={1} />
+        <directionalLight position={[-3, 0, 0]} intensity={1} />
+        <directionalLight position={[0, 0, 3]} intensity={1} />
+        <directionalLight position={[0, 0, -3]} intensity={1} />
+        <OrbitControls  autoRotateSpeed={4} />
         <Bearing />
       </Canvas>
     </div>
